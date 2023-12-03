98458a7ea9cccc3c449c99fc59b3d28ddd02c539
Kevin CT <kcayo6155@uta.edu.ec>
2023-12-03 13:42:35 -0500

    feat: Implementa los servicios para el modulo usuario - crud

--- src/users/user.controller.js
+++ src/users/user.controller.js
@@ -1,6 +1,32 @@
 const { sign } = require("jsonwebtoken");
-const { getUserByUser } = require("./user.service");
+const {
+  create,
+  getUserByUser,
+  getUserData,
+  getUserById,
+  updateUser,
+  deleteUser,
+} = require("./user.service");
 const { genSaltSync, hashSync, compareSync } = require("bcrypt");
+
+const createUser = (req, res) => {
+  const body = req.body;
+  const salt = genSaltSync(10);
+  body.pass_usu = hashSync(body.pass_usu, salt);
+  create(body, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        success: 0,
+        message: "Database connection errror",
+      });
+    }
+    return res.status(200).json({
+      success: 1,
+      data: results,
+    });
+  });
+};
 const login = (req, res) => {
   const body = req.body;
   const user_usu = body.user_usu;
@@ -36,6 +62,81 @@ const login = (req, res) => {
     }
   });
 };
+
+const getUsers = (req, res) => {
+  getUserData((err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    return res.json({
+      success: 1,
+      data: results,
+    });
+  });
+};
+
+const getUser = (req, res) => {
+  const id_usu = req.params.id_usu;
+  getUserById(id_usu, (err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    if (!results) {
+      return res.json({
+        success: 0,
+        message: "Record not Found",
+      });
+    }
+    return res.json({
+      success: 1,
+      data: results,
+    });
+  });
+};
+
+const updateUsers = (req, res) => {
+  const body = req.body;
+  const id_usu = req.params.id_usu;
+  const salt = genSaltSync(10);
+  body.pass_usu = hashSync(body.pass_usu, salt);
+  updateUser(id_usu, body, (err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    return res.json({
+      success: 1,
+      message: "updated successfully",
+    });
+  });
+};
+
+const deleteUsers = (req, res) => {
+  const id_usu = req.params.id_usu;
+  deleteUser(id_usu, (err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    /* if (!results) {
+      return res.json({
+        success: 0,
+        message: "Record not found",
+      });
+    }*/
+    return res.json({
+      success: 1,
+      message: "user deleted successfully",
+    });
+  });
+};
 module.exports = {
   login,
+  createUser,
+  getUsers,
+  getUser,
+  updateUsers,
+  deleteUsers,
 };
--- src/users/user.service.js
+++ src/users/user.service.js
@@ -1,5 +1,19 @@
 const pool = require("../config/database");
 
+const create = (data, callBack) => {
+  pool.query(
+    `insert into usuarios(user_usu, pass_usu, rol_usu) 
+                values(?,?,?)`,
+    [data.user_usu, data.pass_usu, data.rol_usu],
+    (error, results, fields) => {
+      if (error) {
+        callBack(error);
+      }
+      return callBack(null, results);
+    }
+  );
+};
+
 const getUserByUser = (user_usu, callBack) => {
   pool.query(
     `select * from usuarios where user_usu = ?`,
@@ -12,6 +26,60 @@ const getUserByUser = (user_usu, callBack) => {
     }
   );
 };
+
+const getUserData = (callBack) => {
+  pool.query(`select * from usuarios`, [], (error, results, fields) => {
+    if (error) {
+      return callBack(error);
+    }
+    console.log("RES :", results);
+    return callBack(null, results);
+  });
+};
+
+const getUserById = (id_usu, callBack) => {
+  pool.query(
+    `select * from usuarios where id_usu = ?`,
+    [id_usu],
+    (error, results, fields) => {
+      if (error) {
+        return callBack(error);
+      }
+      return callBack(null, results[0]);
+    }
+  );
+};
+
+const updateUser = (id_usu, user, callBack) => {
+  pool.query(
+    `update usuarios set user_usu=?, pass_usu=?, rol_usu=? where id_usu=?`,
+    [user.user_usu, user.pass_usu, user.rol_usu, id_usu],
+    (error, results, fields) => {
+      if (error) {
+        return callBack(error);
+      }
+      return callBack(null, results);
+    }
+  );
+};
+
+const deleteUser = (id_usu, callBack) => {
+  pool.query(
+    `delete from usuarios where id_usu=?`,
+    [id_usu],
+    (error, results, fields) => {
+      if (error) {
+        return callBack(error);
+      }
+      return callBack(null, results[0]);
+    }
+  );
+};
 module.exports = {
   getUserByUser,
+  create,
+  getUserData,
+  getUserById,
+  updateUser,
+  deleteUser,
 };
