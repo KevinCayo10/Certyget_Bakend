b4a87c4cdf9c6fe8988ade227ae8e16bd5a1dcbe
Kevin CT <kcayo6155@uta.edu.ec>
2023-12-16 23:03:57 -0500

    feat: Implementa los servicios para registrar participantes y certificados

--- src/certificados/certificados.route.js
+++ src/certificados/certificados.route.js
@@ -4,11 +4,15 @@ const {
   getDetalleCursosInstructores,
   getCertificadosByCursos,
   getCertificados,
+  registerParticipantes,
+  registerCertificado,
 } = require("./certificados.controller");
 const upload = multer({ storage: multer.memoryStorage() });
 
 routerCertificados.get("/detalle/:id_cur", getDetalleCursosInstructores);
 routerCertificados.get("/:id_cur", getCertificadosByCursos);
 routerCertificados.get("/", getCertificados);
+routerCertificados.post("/participantes/", registerParticipantes);
+routerCertificados.post("/", upload.single("certificado"), registerCertificado);
 
 module.exports = routerCertificados;
--- src/certificados/certificados.service.js
+++ src/certificados/certificados.service.js
@@ -44,13 +44,12 @@ const getCertificadoByIdCursos = (id_cur, callBack) => {
 // query para el registro de certificados
 const createCertificado = (data, callBack) => {
   pool.query(
-    `insert into generar_certificados(url_gen_cer,estado_cer,fecha_gen_cer, ced_par_cer, id_cur,id_cur_cer)`,
+    `insert into generar_certificados(url_gen_cer,estado_cer,fecha_gen_cer, ced_par_cer,id_cur_cer) values(?,?,?,?,?)`,
     [
       data.url_gen_cer,
       data.estado_cer,
       data.fecha_gen_cer,
       data.ced_par_cer,
-      data.id_cur,
       data.id_cur_cer,
     ],
     (error, results, fields) => {
@@ -110,6 +109,33 @@ const getDetalleInstructoresByIdCurso = (id_cur, callBack) => {
   );
 };
 // query para la verificación de duplicación de certificados
+const createParticipantes = (data, callBack) => {
+  // Mapea el array de participantes y crea un array de valores para cada participante
+  const participantesValues = data.map((participante) => [
+    participante.ced_par,
+    participante.nom_pat_par,
+    participante.nom_mat_par,
+    participante.ape_pat_par,
+    participante.ape_mat_par,
+    participante.telf_par,
+    participante.email_par,
+    participante.dir_par,
+    participante.carrera_par,
+    participante.fac_par,
+    participante.uni_par,
+  ]);
+
+  // Query SQL para la inserción múltiple
+  const sql = `INSERT INTO participantes (ced_par, nom_pat_par, nom_mat_par, ape_pat_par, ape_mat_par, telf_par, email_par, dir_par, carrera_par, fac_par, uni_par) VALUES ?`;
+
+  // Ejecuta la consulta
+  pool.query(sql, [participantesValues], (error, results, fields) => {
+    if (error) {
+      return callBack(error);
+    }
+    return callBack(null, results);
+  });
+};
 
 module.exports = {
   getCertificadoByIdCursos,
@@ -118,4 +144,5 @@ module.exports = {
   getDetalleInstructoresByIdCurso,
   getCertificadoByIdCursos,
   getCertificadoData,
+  createParticipantes,
 };
